ecacef1bc13c0d571ecc94e04a9bee23fcfa3783
Eric Williams <eric@churchwilliams.com>
2015-09-09 14:20:42 -0600

    finished. Used  to mainpulate data before it reaches controller

--- service.js
+++ service.js
@@ -1,12 +1,27 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
+
+  // this.getUsers = function() {
+  //   return $http({
+  //       method: 'GET',
+  //       url: 'http://reqr.es/api/users?page=1'
+  //   })
+  // }
 
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
-    })
-  }
+	var deferred = $q.defer();
+    $http.get('http://reqr.es/api/users?page=1')
+    	.then(function(res){
+    		var parsedResponse = res.data.data;
+    		for (var i = 0; i < parsedResponse.length; i++) {
+    			parsedResponse[i].first_name = 'Ralf'
+    		}
+
+    		deferred.resolve(parsedResponse);
+   	});
+
+    	return deferred.promise;
+  };
 
 });
